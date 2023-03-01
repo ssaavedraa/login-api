fd9ba59586bbd437badb75a3e36ef85334a3d6fb
ssaavedraa <=>
2023-03-01 15:05:04 -0500

    create user router

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+import { CreateUserDto } from '../validators/user.create.validator'
+import { userController } from '../controllers/index'
+import { ValidationMiddleware } from './../middlewares/validation.middleware'
+
+const userRouter = Router()
+
+userRouter.post('/', ValidationMiddleware(CreateUserDto), userController.createUser)
+
+export default userRouter
