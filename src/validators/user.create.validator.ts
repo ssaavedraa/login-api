5528a42c900f1cc8518c326f69036afb48be91f3
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-03-03 05:11:21 -0500

    validate email as email

--- src/validators/user.create.validator.ts
+++ src/validators/user.create.validator.ts
@@ -1,7 +1,8 @@
-import { IsString } from 'class-validator'
+import { IsEmail, IsString } from 'class-validator'
 
 export class CreateUserDto {
   @IsString()
+  @IsEmail()
   public email: string
 
   @IsString()
