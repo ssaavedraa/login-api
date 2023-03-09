338fbdd872edf210fa9fb6e6f70c93079d56bd50
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-03-08 23:35:37 -0500

    test uthentication middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express'
 import { CreateUserDto } from '../validators/user.create.validator'
 import { userController } from '../controllers/index'
 import { ValidationMiddleware } from './../middlewares/validation.middleware'
+import { authMiddleware } from '../middlewares/auth.middleware'
 
 const userRouter = Router()
 
 userRouter.post('/', ValidationMiddleware(CreateUserDto), userController.createUser)
+userRouter.get('/test', authMiddleware, userController.checkIfUserExists)
 userRouter.get('/', userController.checkIfUserExists)
 
 export default userRouter
