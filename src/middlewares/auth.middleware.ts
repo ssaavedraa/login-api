3ef785b1a10588d781d7feb40f186e8187e39d39
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-03-09 08:48:55 -0500

    throw new exceptions and update token validation

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,18 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import { JwtPayload, verify } from 'jsonwebtoken'
+import { ForbiddenExcception } from '../httpExceptions/forbidden.exception'
+import { UnauthorizedException } from '../httpExceptions/unauthorized.exception'
 
 export function authMiddleware (req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization
 
   if (!authHeader) {
-    return res.sendStatus(401)
+    throw new UnauthorizedException('Authorization header not provided')
   }
 
   const token = authHeader.split(' ')[1]
 
-  verify(token, process.env.REFRESH_TOKEN_SECRET, (error, decoded) => {
+  verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
     if (error) {
-      return res.sendStatus(403)
+      throw new ForbiddenExcception('Invalid access Token')
     }
 
     console.log((decoded as JwtPayload).email)
